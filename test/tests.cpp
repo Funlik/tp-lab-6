55638776a486a92ab54f2f339877416c40ffd423
Anton Korkunov <antonkorkunov@MacBook-Air-Anton.local>
2020-03-04 19:10:38 +0300

    fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -31,7 +31,8 @@ Project NewProject1("TEST2",1000);
 TeamLeader Lead1(0.4,200,2,"TESTOVA VIKA TESTOVNA",0,"TeamLeader",2,100);
 Lead1.setWorktime(10);
 Lead1.Add_to_Project(&NewProject1);
-EXPECT_EQ(2600,Lead1.Calculate());
+double temp = 2*100+0.4*1000+200*10;
+EXPECT_EQ(temp,Lead1.Calculate());
 }
 
 TEST(testProject, init4) {
@@ -64,5 +65,6 @@ TEST(testOnlyProject, init7) {
 Project NewProject1("TEST4",10000);
 Manager newMan(0.7 ,0,"K",20,"Manager");
 NewProject1.AddManager(&newMan);
-EXPECT_EQ(10000*0.7,newMan.Calculate());
+double temp = 10000*0.7;
+EXPECT_EQ(temp,newMan.Calculate());
 }
