99c6f995835923199af1965a9b743a301f2027b7
Valerii <valerider@icloud.com>
2020-03-04 15:19:15 +0300

    tests fixed(3)

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,30 +1,30 @@
 #include "gtest/gtest.h"
 #include "Employees.h"
 
-TEST(test, id) {
+TEST(testPersonal, id) {
 	Driver driver(12, "name", "Driver", 15);
 	EXPECT_EQ(12, driver.GetId());
 }
 
-TEST(test, payment) {
+TEST(testPersonal, payment) {
 	Driver driver(12, "name", "Driver", 15);
 	driver.AddDone();
 	EXPECT_EQ(15, driver.CalcPay());
 }
 
-TEST(test, name) {
+TEST(testPersonal, name) {
 	Driver driver(12, "name", "Driver", 15);
 	EXPECT_EQ("driver", driver.GetName());
 }
 
-TEST(test, payment) {
+TEST(testProgrammer, payment) {
 	Project project("project", 0.0003, 0.00034, 0.0006);
 	Programmer programmer(12, "name", "Programmer", 25, &project);
 	programmer.AddDone();
 	EXPECT_EQ(48, programmer.CalcPay());
 }
 
-TEST(test, payment) {
+TEST(testSeniorManager, payment) {
 	SeniorManager manager(12, "name", "SeniorManager", 80, "Main", 14);
 	manager.AddDone();
 	EXPECT_EQ(224, manager.CalcPay());
