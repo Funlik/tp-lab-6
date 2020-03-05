f6dd2bdd6af33fe64c0aa27e9a0230f3cbea6506
Ilya Solovyov <ilyasolovyov52@yandex.ru>
2020-03-05 09:47:37 +0300

    I'm testing test

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,21 +1,20 @@
 #include "gtest/gtest.h"
-//
-//#include "General.h"
-//#include "Engineer.h"
-//#include "Manager.h"
-//#include "Personal.h"
-//#include "WorkersFabric.h"
-//#include "Interfaces.h"
-//
+
+#include "General.h"
+#include "Engineer.h"
+#include "Manager.h"
+#include "Personal.h"
+#include "WorkersFabric.h"
+#include "Interfaces.h"
+
 //#include <vector>
 //#include <string>
-//
-//TEST(Tests, TestCleaner) {
-//    Cleaner *cleaner = new Cleaner(1,"Ivanov Ivan Ivanovich", 5, 100)
-//    EXPECT_EQ(cleaner->getPayment(), 500.0);
-//}
-//
-//
+
+TEST(Tests, TestCleaner) {
+    Cleaner *cleaner = new Cleaner(1,"Ivanov Ivan Ivanovich", 5, 100);
+    EXPECT_EQ(cleaner->getPayment(), 500.0);
+}
+
 //TEST(Tests, TestFabric) {
 //    vector<string> first{"23", "Рябова", "Ирма", "Созоновна", "tester", "30", "50", "proj2", "170000", "0.1"};
 //    vector<string> second{"4", "Белоусова", "Фия", "Вениаминовна", "driver", "30", "700"};
@@ -26,9 +25,4 @@
 //    ASSERT_NE(workers[0], nullptr);
 //    ASSERT_NE(workers[1], nullptr);
 //}
-
-
-TEST(Tests, TestFabric) {
-    ASSERT_EQ(0, 0);
-}
 
