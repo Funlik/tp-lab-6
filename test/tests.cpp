cffd01a1a9dedf04607c1aec072ea6393452743a
Sergey Baranenkov <baranenkovs@mail.ru>
2020-03-05 23:28:04 +0300

    Добавлен 1 тест

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,9 @@
 #include "gtest/gtest.h"
+#include "WorkerFactory.h"
+
+TEST(Payment_test, Employee_payment_test) {
+    Employee * emp = WorkerFactory::makeDriver("Driver",1,"test",30, 2);
+    emp->get_salary();
+    EXPECT_EQ(emp->get_current_payment(), 60);
+}
+
