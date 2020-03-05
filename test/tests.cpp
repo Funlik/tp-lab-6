7230d3f1a0693d8a148e84a9724b4e99856cc7c3
Ilya Solovyov <ilyasolovyov52@yandex.ru>
2020-03-05 09:50:30 +0300

    Second test

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,22 +7,22 @@
 #include "WorkersFabric.h"
 #include "Interfaces.h"
 
-//#include <vector>
-//#include <string>
+#include <vector>
+#include <string>
 
 TEST(Tests, TestCleaner) {
     Cleaner *cleaner = new Cleaner(1,"Ivanov Ivan Ivanovich", 5, 100);
     EXPECT_EQ(cleaner->getPayment(), 500.0);
 }
 
-//TEST(Tests, TestFabric) {
-//    vector<string> first{"23", "Рябова", "Ирма", "Созоновна", "tester", "30", "50", "proj2", "170000", "0.1"};
-//    vector<string> second{"4", "Белоусова", "Фия", "Вениаминовна", "driver", "30", "700"};
-//
-//    vector<Employee*> workers;
-//    workers.push_back(createWorker(first));
-//    workers.push_back(createWorker(second));
-//    ASSERT_NE(workers[0], nullptr);
-//    ASSERT_NE(workers[1], nullptr);
-//}
+TEST(Tests, TestFabric) {
+    vector<string> first{"23", "Рябова", "Ирма", "Созоновна", "tester", "30", "50", "proj2", "170000", "0.1"};
+    vector<string> second{"4", "Белоусова", "Фия", "Вениаминовна", "driver", "30", "700"};
+
+    vector<Employee*> workers;
+    workers.push_back(createWorker(first));
+    workers.push_back(createWorker(second));
+    ASSERT_NE(workers[0], nullptr);
+    ASSERT_NE(workers[1], nullptr);
+}
 
