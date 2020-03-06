59890bb660b291bc9b60e3cad084f474a097e1a9
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2020-03-07 00:54:34 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,20 +4,20 @@
 TEST(Test, TestNum) 
 {
   Factory f;
-  f.readWorkers("workers.txt");
+  f.readWorkers("/test/workers.txt.txt");
   EXPECT_EQ(f.getNum(), 20);
 }
 
 TEST(Test, TestGetID) 
 {
   Factory f;
-  f.readWorkers("workers.txt");
+  f.readWorkers("/test/workers.txt.txt");
   EXPECT_EQ((f.getCleaners()[0]).getId(), 1);
 }
 
 TEST(Test, TestGetName) 
 {
   Factory f;
-  f.readWorkers("workers.txt");
+  f.readWorkers("/test/workers.txt.txt");
   EXPECT_EQ((f.getCleaners()[0]).getName(), "Cleaner_1");
 }
