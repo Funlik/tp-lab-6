a05ddb0ea6852956f87280ad7430d0bb7e40fe82
Polyukhov Vladislav <vlad.polyukhov@gmail.com>
2020-03-06 20:18:31 +0300

    fix tests.cpp 20:18

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,22 +2,22 @@
 #include "../include/Factory.h"
 
 TEST(Staff, task_1) {
-	Driver employee(9, "Vasya", 1, 150);
+	Driver employee(9, "Vasya", 150);
 	EXPECT_EQ(9, employee.getID());
 }
 
 TEST(Staff, task_2) {
-	Cleaner employee(1, "Petya", 0, 100);
+	Cleaner employee(1, "Petya", 100);
 	employee.setWorkTime(40);
 	EXPECT_EQ(40, employee.getWorkTime());
 }
 
 TEST(Staff, task_3) {
-	Cleaner employee(1, "Kolya", 0, 100);
+	Cleaner employee(1, "Kolya", 100);
 	EXPECT_EQ("Petya", employee.getName());
 }
 
 TEST(Staff, task_4) {
-	SeniorManager employee(99, "Vasilich", 6, 524000, 20, 100);
+	SeniorManager employee(99, "Vasilich", 524000, 20, 100);
 	EXPECT_EQ(524000, employee.getBudget());
 }
