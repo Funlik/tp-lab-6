267bd03f89e2f421523149a33dbb90ccdb739692
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2020-03-06 23:57:38 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,23 @@
 #include "gtest/gtest.h"
+#include "Factory.h"
+
+TEST(CalculationsTests, TestNum) 
+{
+  Factory f;
+  f.readWorkers("workers.txt");
+  EXPECT_EQ(f..getNum(), 20);
+}
+
+TEST(CalculationsTests, TestGetID) 
+{
+  Factory f;
+  f.readWorkers("workers.txt");
+  EXPECT_EQ((f.getCleaners()[0]).getId(), 1);
+}
+
+TEST(CalculationsTests, TestGetName) 
+{
+  Factory f;
+  f.readWorkers("workers.txt");
+  EXPECT_EQ((f.getCleaners()[0]).getId(), "Cleaner_1");
+}
