1aa949c1224375b4d528930ea5d8f16cfaf243af
Polyukhov Vladislav <vlad.polyukhov@gmail.com>
2020-03-06 20:37:55 +0300

    fix tests.cpp 20:37

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,7 +2,7 @@
 #include "../include/Factory.h"
 
 TEST(Staff, task_1) {
-	Driver employee(9, "Vasya", 150);
+	Driver employee(9, "Vasya", 150, 15);
 	EXPECT_EQ(9, employee.getID());
 }
 
@@ -18,6 +18,6 @@ TEST(Staff, task_3) {
 }
 
 TEST(Staff, task_4) {
-	SeniorManager employee(99, "Vasilich", 524000, 20, 100);
+	SeniorManager employee(99, "Vasilich", "-" ,524000, 20, 100);
 	EXPECT_EQ(524000, employee.getBudget());
 }
