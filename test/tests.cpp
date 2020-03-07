c228cb181dcefe87f1f121ff46ea270e61c71e7e
dselivanovskaya <selivanovskaya.dasha@mail.ru>
2020-03-08 00:18:27 +0300

    third try

--- test/tests.cpp
+++ test/tests.cpp
@@ -22,14 +22,14 @@ TEST(Lab6Test2, DriverTest) {
 TEST(Lab6Test3, ProgrammerTest) {
 	Factory* office = new Factory();
 	Employee* programmer = office->NewEmployee("Programmer", 3, "Sara", 30, 500, 0);
-	programmer->CalculatePayment(5, 30000);
+	((Programmer*)programmer)->CalculatePayment(5, 30000);
 	EXPECT_EQ(programmer->GetPayment(), 16500);
 }
 
 TEST(Lab6Test4, TesterTest) {
 	Factory* office = new Factory();
 	Employee* tester = office->NewEmployee("Tester", 4, "Mike", 25, 400, 0);
-	tester->CalculatePayment(4, 30000);
+	((Tester*)tester)->CalculatePayment(4, 30000);
 	EXPECT_EQ(tester->GetPayment(), 11200);
 }
 
@@ -40,14 +40,14 @@ TEST(Lab6Test5, TeamLeaderTest) {
 	Employee* programmer2 = office->NewEmployee("Programmer", 2, "Kat", 30, 500, 0);
 	teamleader->AddProgrammer(programmer1);
 	teamleade->AddProgrammer(programmer2);
-	teamleader->CalculatePayment(5, 30000, 300);
+	((TeamLeader*)teamleader)->CalculatePayment(5, 30000, 300);
 	EXPECT_EQ(teamleader->GetPayment(), 20100);
 }
 
 TEST(Lab6Test6, ManagerTest) {
 	Factory* office = new Factory();
 	Employee* manager = office->NewEmployee("Manager", 6, "Jackson", 25, 700, 20);
-	manager->CalculatePayment(20, 30000);
+	((Manager*)manager)->CalculatePayment(20, 30000);
 	EXPECT_EQ(manager->GetPayment(), 6000);
 }
 
@@ -58,7 +58,7 @@ TEST(Lab6Test7, ProjectManagerTest) {
 	Employee* manager2 = office->NewEmployee("Manager", 2, "Jacob", 25, 700, 10);
 	projectmanager->AddManager(manager1);
 	projectmanager->AddManager(manager2);
-	projectmanager->CalculatePayment(20, 30000, 350);
+	((ProjectManager*)projectmanager)->CalculatePayment(20, 30000, 350);
 	EXPECT_EQ(projectmanager->GetPayment(), 6700);
 }
 
@@ -69,6 +69,6 @@ TEST(Lab6Test8, SeniorManagerTest) {
 	Employee* prmanager2 = office->NewEmployee("ProjectManager", 2, "Katrin", 30, 800, 15);
 	seniormanager->AddProjMan(prmanager1);
 	seniormanager->AddProjMan(prmanager2);
-	seniormanager->CalculatePayment(23, 30000, 400);
+	((SeniorManager*)seniormanager)->CalculatePayment(23, 30000, 400);
 	EXPECT_EQ(seniormanager->GetPayment(), 7700);
 }
