6334c4425ab10df8a8c486487a9885306cb48cb8
Andrey Noskov <aanoskov@edu.hse.ru>
2020-03-08 23:19:50 +0300

    second

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,21 +5,21 @@
 
 TEST(lab6, task1)
 {
-	Driver* emp = new Driver("Alex", 77, "Driver", 1.5);
+	Driver* emp = new Driver("Alex", 77, 300, "Driver", 1.5);
 	unsigned int res = emp->getId();
 	EXPECT_EQ(77, res);
 }
 
 TEST(lab6, task2)
 {
-	Driver* emp = new Driver("Alex", 78, "Driver", 1.5);
+	Driver* emp = new Driver("Alex", 78, 300, "Driver", 1.5);
 	string res = emp->getName();
 	EXPECT_EQ("Alex", res);
 }
 
 TEST(lab6, task3)
 {
-	Driver* emp = new Driver("Alex", 79, "Driver", 1.5);
+	Driver* emp = new Driver("Alex", 79, 300, "Driver", 1.5);
 	emp->setWorkTime(40);
 	unsigned int res = emp->getWorkTime();
 	EXPECT_EQ(40, res);
