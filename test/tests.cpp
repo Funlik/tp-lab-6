682af7245139b2e65877b21202a0749ebc49b748
unknown <nastenckavoronowa@yandex.ru>
2020-03-09 17:00:52 +0300

    second

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,14 +8,14 @@
 #include<vector>
 #include <algorithm>
 
-TEST(payment, TestCleaner)
+TEST(lab6, task1)
 {
 	Cleaner clean(1, "Hart_Matthew", 40, 50);
 	
 	
 	EXPECT_EQ(clean->getPayment(), 2000);
 }
-TEST(payment, TestDriver)
+TEST(lab6, task2)
 {
 	Driver drive(1, "Boyd_John", 40, 70);
 
@@ -23,7 +23,7 @@ TEST(payment, TestDriver)
 	EXPECT_EQ(drive->getPayment(), 2800);
 }
 
-TEST(payment, TestProgrammer)
+TEST(lab6, task3)
 {
 	Programmer prog(20, "Rich_Preston", 40, 100, 0.1, "Project1", 1000000);
 
@@ -31,7 +31,7 @@ TEST(payment, TestProgrammer)
 	EXPECT_EQ(prog->getPayment(), 104000);
 }
 
-TEST(payment, TestTester)
+TEST(lab6, task4)
 {
 	Tester prog(24, "Harmon_Oliver", 40, 100, 0.1, "Project1", 1000000);
 	
@@ -39,7 +39,7 @@ TEST(payment, TestTester)
 	EXPECT_EQ(prog->getPayment(), 104000);
 }
 
-TEST(payment, TestTeamleader)
+TEST(lab6, task5)
 {
 	TeamLeader prog(27, "Williamson_Duane", 40, 100, 0.1, "Project1", 1000000, 4);
 
@@ -47,7 +47,7 @@ TEST(payment, TestTeamleader)
 	EXPECT_EQ(prog->getPayment(), 104200);
 }
 
-TEST(payment, TestManager)
+TEST(lab6, task6)
 {
 	Manager mag(14,"Goodman_Ferdinand", 40, 0.1,"Project1", 1000000);
 
@@ -55,7 +55,7 @@ TEST(payment, TestManager)
 	EXPECT_EQ(mag->getPayment(), 100000);
 }
 
-TEST(payment, TestProjectManager)
+TEST(lab6, task7)
 {
 	ProjectManager mag(11, "Hutchinson_Mark", 40, 0.1, "Project1", 1000000, 7);
 
@@ -63,7 +63,7 @@ TEST(payment, TestProjectManager)
 	EXPECT_EQ(mag->getPayment(), 100000);
 }
 
-TEST(payment, TestSeniorManager)
+TEST(lab6, task8)
 {
 	SeniorManager mag(10, "Franklin_Brian", 40, 0.1, "Project1", 1000000, 26);
 
