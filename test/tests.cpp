9689254f50b5afee3772a3f8d4ceffe926190df2
rainbowsock <rainbow_sock@mail.ru>
2020-03-09 20:43:17 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,36 +8,18 @@
 #include "Personal.h"
 #include "CompanyCreation.h"
 
-TEST(test1, CleanerPaymentTest) 
+TEST(test1, CleanerIDTest)
 {
 	CompanyCreation* company = new CompanyCreation();
-	Employee* cleaner = company->addEmployee(1, "Cleaner", "Taylor", 40, 50, 0);
+	Employee* cleaner = company->addEmployee(1, "Cleaner", "Oliver", 40, 50, 0);
 
-	cleaner->calcPayment();
-	EXPECT_EQ(cleaner->getPayment(), 2000);
+	EXPECT_EQ(cleaner->getID(), 1);
 }
 
-TEST(test2, DriverPaymentTest) 
+TEST(test2, DriverIDTest)
 {
 	CompanyCreation* company = new CompanyCreation();
-	Employee* driver = company->addEmployee(2, "Driver", "Karl", 50, 50, 0);
+	Employee* driver = company->addEmployee(2, "Driver", "Mark", 50, 50, 0);
 
-	driver->calcPayment();
-	EXPECT_EQ(driver->getPayment(), 2500);
-}
-
-TEST(test3, CleanerIDTest)
-{
-	CompanyCreation* company = new CompanyCreation();
-	Employee* cleaner = company->addEmployee(3, "Cleaner", "Oliver", 40, 50, 0);
-
-	EXPECT_EQ(cleaner->getID(), 3);
-}
-
-TEST(test4, DriverIDTest)
-{
-	CompanyCreation* company = new CompanyCreation();
-	Employee* driver = company->addEmployee(4, "Driver", "Mark", 50, 50, 0);
-
-	EXPECT_EQ(driver->getID(), 4);
+	EXPECT_EQ(driver->getID(), 2);
 }
