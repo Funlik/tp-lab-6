0316e39a1402a3aeb7f96a35386e5fdcee13cb08
Katya Malysheva <KatyaMalysheva2000@yandex.ru>
2020-03-10 01:54:57 +0300

    add tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,22 @@
 #include "gtest/gtest.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <string>
+#include"Company.h"
+#include "emploee.h"
+#include "Engineers.h"
+#include "Interfaces.h"
+#include "Managers.h"
+#include "Personals.h"
+TEST(task1, first cleaner) {
+	Cleaner me(1, "Kate Malysheva", 200);
+
+	me.setTime(40);
+	me.CalcPayment();
+
+	
+	EXPECT_EQ(8000, me.getPayment());
+}
+}
