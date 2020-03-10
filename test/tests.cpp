daf5e19747130d386eaee0c40a8468f029a0a5a9
Artem Kuryakov <kurik01tagil@gmail.com>
2020-03-10 21:57:31 +0300

    added headers

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,1 +1,22 @@
-#include "gtest/gtest.h"
+//#include "pch.h"
+#include "Cleaner.h"
+#include "Employee.h"
+#include "Engineer.h"
+#include "Heading.h"
+#include "Job.h"
+#include "Manager.h"
+#include "Personal.h"
+#include "Progect.h"
+#include "ProjectManager.h"
+#include "SeniorManager.h"
+#include "Programmer.h"
+#include "Tester.h"
+#include "WorkTime.h"
+#include "TeamLeader.h"
+
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
