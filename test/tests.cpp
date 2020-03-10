d371ce848b919bc8bb713119ab4384a2f068c4c7
Polina Demochkina <lollipoli@live.com>
2020-03-10 18:01:19 +0300

    changed expected value in tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,7 +5,7 @@
 TEST(task1, case_1) {
 	Projects* project = new Projects("Test", 1000000);
 	TeamLeader* team_lead = new TeamLeader(1, "Mikhail Zhelezin", "teamLeader", 40, 250, 0.1, project, 100);
-    EXPECT_EQ(team_lead->GetPosition(), 110000);
+    EXPECT_EQ(team_lead->GetPosition(), "teamLeader");
 }
 
 TEST(task1, case_2) {
