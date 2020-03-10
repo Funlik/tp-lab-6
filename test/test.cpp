9ff53206e7e51043a5a3f2b1f4a05abd224da103
Klenkov <50375131+Klenkov@users.noreply.github.com>
2020-03-11 01:46:41 +0300

    nightstalker

--- test/test.cpp
+++ test/test.cpp
@@ -12,15 +12,15 @@
 
 TEST(Tests, firstTest){
     Driver *driver = new Driver(1, "Andrey Klenkov Dmitrievich", 30, 500);
-    EXPECT_Q(driver->getPayment(), 15000);
+    EXPECT_EQ(driver->getPayment(), 15000);
 }
 
-//TEST(Tests, secondTest){
-//    SeniorManager *seniorManager = new SeniorManager(1, "Andrey Klenkov Dmitrievich", 40, 1000, 2, 3000);
-//    EXPECT_Q(seniorManager->getPayment(), 52000);
-//}
+TEST(Tests, secondTest){
+    SeniorManager *seniorManager = new SeniorManager(1, "Andrey Klenkov Dmitrievich", 40, 1000, 2, 3000);
+    EXPECT_EQ(seniorManager->getPayment(), 52000);
+}
 
 TEST(Tests, thirdTest){
     TeamLeader *teamLeader = new TeamLeader(1, "Andrey Klenkov Dmitrievich", 35, 1500, "project", 50000, 0.4, 2, 6000);
-    EXPECT_Q(teamLeader->getPayment(), 84500);
+    EXPECT_EQ(teamLeader->getPayment(), 84500);
 }
