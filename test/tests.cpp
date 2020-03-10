05a5ab2f7956c6ab23356df144f7cff4c5950248
Krylova Maria <krylova.maria.2629@gmail.com>
2020-03-10 23:28:26 +0300

    change tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,11 +5,13 @@
 TEST(task1, case_1) {
     Cleaner* cleaner = new Cleaner("1", "Ivanov Ivan");
     cleaner->setWorktime(40);
+    cleaner->calc();
     EXPECT_EQ(cleaner->getPayment(), 8000);
 }
 
 TEST(task1, case_2) {
     SeniorManager* manager = new SeniorManager("2", "Gavrilov Kirill", Projects::fourth, 15);
     manager->setWorktime(40);
-EXPECT_EQ(manager->getPayment(), 37500);
+    cleaner->calc();
+    EXPECT_EQ(manager->getPayment(), 37500);
 }
