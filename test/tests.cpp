06c660359e7cd8e01f96e8eb3f300d4562788533
Mikhail Krivdin <xhazker@gmail.com>
2020-03-11 02:45:27 +0300

    first

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,19 +1,19 @@
 #include "gtest/gtest.h"
 #include "../include/Company.h"
 
-TEST(lab6, task1)
+TEST(testCleaner, task1)
 {
 	Cleaner *c = new Cleaner(1, "Test", 120, 500);
 	EXPECT_EQ(c->payroll(), 60000);
 }
 
-TEST(lab6, task2)
+TEST(testEngineer, task2)
 {
-	Engineer *e = new Driver(2, "Test2", 130, 500);
+	Engineer *e = new Engineer(2, "Test2", 130, 500);
 	EXPECT_EQ(e->payroll(), 65000);
 }
 
-TEST(lab6, task3)
+TEST(testProgrammer, task3)
 {
 	Programmer *p = new Programmer(3, "Test3", 150, 500);
 	EXPECT_EQ(p->payroll(), 75000);
