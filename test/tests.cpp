a4260368a60e348b0d46c20b745cf8a148c222a2
Dasha <wqe>
2020-03-12 20:39:17 +0300

    fix test file

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,5 +3,5 @@
 TEST(lab6_, test0){
     Programmer* programmer = new Programmer(8,"Jacobs Esmond",40,"programmer",30,2,1000);
     programmer->calc();
-    EXPECT_EQ(programmer-getPayment(), 4220);
+    EXPECT_EQ(programmer->getPayment(), 4220);
 }
